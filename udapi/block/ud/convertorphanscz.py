d835eb64ddfaedd6e4b919d86054e9d57b0de405
Dan Zeman <zeman@ufal.mff.cuni.cz>
2018-02-04 15:05:57 +0100

    Trying to adapt from English to Czech.

--- udapi/block/ud/convertorphanscz.py
+++ udapi/block/ud/convertorphanscz.py
@@ -7,7 +7,7 @@ from udapi.core.block import Block
 class ConvertOrphansCz(Block):
     """Converts full sentences into artificial.
         Usage: cat file-to-convert.conllu | udapy -s ud.ConvertOrphansCz
-        Notes: 
+        Notes:
         If a sentence is processed, this information is added to the misc column:
             'Processed=Yes'marker is added to the node that will be deleted
              new heads and labels are added to nodes that should be rehung ('newNode=new-head:relation')
@@ -16,17 +16,20 @@ class ConvertOrphansCz(Block):
     """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.subst = {'he': 'she', 'she': 'he', 'i':'you', 'you': 'they', 'they': 'you', 'we': 'you'}
+        # Substitution of pronouns is probably useless because Czech is a pro-drop language.
+        self.subst = {'on': 'ona', 'ona': 'on', 'já':'ty', 'vy': 'oni', 'oni': 'vy', 'my': 'vy'}
+        # Auxiliary children to remove if parent is elided. (DZ: I don't know why it is called core_ellipsis.)
         self.core_ellipsis = {'aux', 'cop', 'compound'}
-        self.none_core_ellipsis = {'nsubj', 'iobj', 'obj', 'aux', 'compound', 'mark', 'cop'}
-        self.cop_form = {'was', 'were', 'is', 'are', "'m", 'am'}
-           
+        # DZ: I don't know why this is called non_core_ellipsis but these are children to be deleted too!
+        self.none_core_ellipsis = {'nsubj', 'obj', 'iobj', 'aux', 'cop', 'compound', 'mark'}
+        self.cop_form = {'byl', 'byla', 'bylo', 'byli', 'byly', 'jsem', 'jsi', 'je', 'jsme', 'jste', 'jsou', 'budu', 'budeš', 'bude', 'budeme', 'budete', 'budou', 'nebyl', 'nebyla', 'nebylo', 'nebyli', 'nebyly', 'nejsem', 'nejsi', 'není', 'nejsme', 'nejste', 'nejsou', 'nebudu', 'nebudeš', 'nebude', 'nebudeme', 'nebudete', 'nebudou'}
+
     def process_node(self, node):
         if 'Mark' in node.misc:
             children_to_delete = []
             children_to_process = []
 
-            # VERB depends on VERB 
+            # VERB depends on VERB
             if node.upos == 'VERB' and node.parent.upos == 'VERB' and \
                node.parent.form not in self.cop_form and node.form not in self.cop_form:
 
@@ -34,12 +37,13 @@ class ConvertOrphansCz(Block):
                 # THINK: here it is also possible to copy a sentence and make two kinds of ellipsis using one sentence
                 if [c.form.lower() for c in node.children if c.deprel.split(':')[0] == 'nsubj'] == \
                    [c.form.lower() for c in node.parent.children if c.deprel.split(':')[0] == 'nsubj']:
+                    # Remove core children (nsubj, obj, iobj) and auxiliaries. Keep oblique arguments and adjuncts.
                     for child in node.children:
-                        if child.deprel.split(':')[0] in self.none_core_ellipsis or \
-                            child.form in {'nt', "n't"} or (child.form == 'not' and child.prev_node.upos == 'AUX'):
+                        if child.deprel.split(':')[0] in self.none_core_ellipsis:
                             children_to_delete.append(child)
                         else:
                             children_to_process.append(child)
+                    # We need two oblique children. One will be promoted. The other will be attached as orphan.
                     if len([elem for elem in children_to_process if elem.deprel not in {'punct', 'cc', 'mark'}]) >= 2:
                         self.rehang(node, children_to_process)
                         for child in children_to_delete:
@@ -48,23 +52,22 @@ class ConvertOrphansCz(Block):
                         # change the word form if needed
                         self.change_form(node)
                         for child in node.children:
-                            if child.deprel.split(':')[0] in self.core_ellipsis or \
-                               child.form in {'nt', "n't"} or (child.form == 'not' and child.prev_node.upos == 'AUX'):
+                            # Remove auxiliaries and other uninteresting children.
+                            if child.deprel.split(':')[0] in self.core_ellipsis:
                                 children_to_delete.append(child)
                             else:
                                 children_to_process.append(child)
                         self.rehang_alternative(node, children_to_process, children_to_delete, deleteNode=True)
 
-                else:
+                else: # subject forms differ
                     for child in node.children:
-                        if child.deprel.split(':')[0] in self.core_ellipsis or \
-                           child.form in {'nt', "n't"} or (child.form == 'not' and child.prev_node.upos == 'AUX') or \
-                           (child.upos == 'PART' and child.deprel == 'mark'):
+                        if child.deprel.split(':')[0] in self.core_ellipsis or:
                             children_to_delete.append(child)
                         else:
                             children_to_process.append(child)
                     self.rehang_alternative(node, children_to_process, children_to_delete, deleteNode=True)
 
+            # Node is not verb. Its parent is verb.
             elif node.parent.upos == 'VERB' and node.parent.form not in self.cop_form:
 
                 if node.upos in {'PROPN', 'NOUN', 'NUM', 'SYM', 'ADJ', 'ADV'}:
@@ -77,8 +80,7 @@ class ConvertOrphansCz(Block):
                         self.change_form(node)
 
                         for child in node.children:
-                            if child.deprel.split(':')[0] in self.none_core_ellipsis or \
-                               child.form in {'nt', "n't"} or (child.form == 'not' and child.prev_node.upos == 'AUX'):
+                            if child.deprel.split(':')[0] in self.none_core_ellipsis:
                                 children_to_delete.append(child)
                             else:
                                 children_to_process.append(child)
@@ -90,18 +92,14 @@ class ConvertOrphansCz(Block):
                                 for child in children_to_delete:
                                     child.misc['newNode'] = 'deleteThis'
                         else:
-                            # pass relaition and the head to the subject
+                            # pass relation and the head to the subject
                             self.rehang_to_subj(node)
-
-                    #else:
-                        #node.misc['Processed'] = 'deleteSentence'
 
             # VERB depends on a clause with 'cop'
             elif (node.parent.upos == 'VERB' and node.parent.form in self.cop_form) or \
                  any(c.deprel in {'cop'} for c in node.parent.children):
                 for child in node.children:
-                    if child.deprel.split(':')[0] in self.none_core_ellipsis or \
-                       child.form in {'nt', "n't"} or (child.form == 'not' and child.prev_node.upos == 'AUX'):
+                    if child.deprel.split(':')[0] in self.none_core_ellipsis:
                         children_to_delete.append(child)
                     else:
                         children_to_process.append(child)
@@ -119,6 +117,11 @@ class ConvertOrphansCz(Block):
                 node.misc['Processed'] = 'No'
 
     def promote_node(self, node, c, children_to_process):
+        """
+        node ... The node that will be deleted.
+        c ...... The child that will be promoted to the position of the deleted node.
+        children_to_process ... All children of the to-be-deleted node.
+        """
         c.misc['newNode'] = str(node.parent.ord) + ':' + str(node.deprel)
         for the_rest in children_to_process:
             if c.ord != the_rest.ord:
@@ -127,11 +130,16 @@ class ConvertOrphansCz(Block):
                 elif the_rest.deprel.split(':')[0] in {'obl', 'advmod'}:
                     the_rest.misc['newNode'] = str(c.ord) + ':orphan'
                 elif the_rest.deprel.split(':')[0] in {'det', 'amod', 'case', 'nmod'}:
-                    continue
+                    continue ###??? DZ: Why?
                 else:
                     the_rest.misc['newNode'] = str(c.ord) + ':ALARM1'
 
     def promote_if_core(self, node, c, children_to_process):
+        """
+        node ... The node that will be deleted.
+        c ...... The child that will be promoted to the position of the deleted node.
+        children_to_process ... All children of the to-be-deleted node.
+        """
         c.misc['newNode'] = str(node.parent.ord) + ':' + str(node.deprel)
         for the_rest in children_to_process:
             if c.ord != the_rest.ord:
@@ -140,13 +148,18 @@ class ConvertOrphansCz(Block):
                 elif the_rest.deprel.split(':')[0] in {'obj', 'obl', 'advmod'}:
                     the_rest.misc['newNode'] = str(c.ord) + ':orphan'
                 elif the_rest.deprel.split(':')[0] in {'det', 'amod', 'case', 'nmod'}:
-                    continue
+                    continue ###??? DZ: Why?
                 else:
                     the_rest.misc['newNode'] = str(c.ord) + ':ALARM2'
         node.misc['newNode'] = str(c.ord) + ':orphan'
-        
+
     def rehang(self, node, children_to_process, c=None):
-        if c is None:        
+        """
+        node ... The node that will be deleted.
+        children_to_process ... Children of the to-be-deleted node.
+        c ...... Child that should be promoted (if we already have a candidate; otherwise it will be selected now).
+        """
+        if c is None:
             for c in children_to_process:
                 if c.deprel.split(':')[0] == 'obl':
                     self.promote_node(node, c, children_to_process)
@@ -190,6 +203,10 @@ class ConvertOrphansCz(Block):
             node.misc['newNode'] = 'ALARM4'
 
     def change_form(self, node):
+        """
+        If two conjoined verbs have identical subjects, we can try to remove the second verb and
+        replace its subject by something else (as in "He will prepare the dinner and !she! [will] [go] home.")
+        """
         wordform = [c.form.lower() for c in node.children if c.deprel.split(':')[0] == 'nsubj'][0]
         if wordform in self.subst:
             new_form = self.subst[[c.form for c in node.children if c.deprel.split(':')[0] == 'nsubj'][0].lower()]
